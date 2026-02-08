93e18c57e9e2d8442223d6b8c757302b6e942f9e
smitrabari <smitrabari18@gmail.com>
2026-02-08 12:55:25 +0530

    update ch2

--- Let Us C/C Instructions/p2.c
+++ Let Us C/C Instructions/p2.c
@@ -7,7 +7,9 @@ int main()
     printf("Enter a Number(upto 5 digits) : ");
     scanf("%d",&n);
     d5=n % 10;
+    // taking modulas to get last digit
     n= n / 10;
+    // removing the last digit by dividing by 10
     d4=n%10;
     n=n/10;
     d3=n%10;
@@ -16,6 +18,7 @@ int main()
     n=n/10;
     d1=n%10;
     revnum=d5*10000+d4*1000+d3*100+d2*10+d1;
-    printf("your Reverse num will be : %d",revnum);
+    // calculating the reverse number
+    printf("The Reverse number will be : %d",revnum);
     return 0;
 }
