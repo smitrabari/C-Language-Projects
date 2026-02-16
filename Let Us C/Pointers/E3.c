87b1c623dc30d2d84a3fb47d8405ad727699a41b
smitrabari <smitrabari18@gmail.com>
2026-02-16 21:37:05 +0530

    Updated Unit 9

--- Let Us C/Pointers/E3.c
+++ Let Us C/Pointers/E3.c
@@ -1,65 +1,49 @@
-#include <stdio.h>
-#include <math.h>
-
-// Function to calculate distance between two points
-double distance(double x1, double y1, double x2, double y2)
-{
-    return sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
-}
-
-// Function to calculate area of triangle using Heron's formula
-double areaTriangle(double x1, double y1,
-                    double x2, double y2,
-                    double x3, double y3)
-{
-    double a = distance(x1, y1, x2, y2);
-    double b = distance(x2, y2, x3, y3);
-    double c = distance(x3, y3, x1, y1);
-
-    double s = (a + b + c) / 2;
-
-    return sqrt(s * (s - a) * (s - b) * (s - c));
-}
-
-// Function to check whether point (x, y) lies inside triangle ABC
-int isInside(double x1, double y1,
-             double x2, double y2,
-             double x3, double y3,
-             double x, double y)
-{
-    double A  = areaTriangle(x1, y1, x2, y2, x3, y3);
-
-    double A1 = areaTriangle(x, y, x2, y2, x3, y3);
-    double A2 = areaTriangle(x1, y1, x, y, x3, y3);
-    double A3 = areaTriangle(x1, y1, x2, y2, x, y);
-
-    if (fabs(A - (A1 + A2 + A3)) < 0.0001)
-        return 1;
-    else
-        return 0;
-}
-
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+float dis(int, int, int, int);
+float ar(int, int, int, int, int, int);
 int main()
 {
-    double x1, y1, x2, y2, x3, y3;
-    double x, y;
-
-    printf("Enter coordinates of A (x1 y1): ");
-    scanf("%lf %lf", &x1, &y1);
-
-    printf("Enter coordinates of B (x2 y2): ");
-    scanf("%lf %lf", &x2, &y2);
-
-    printf("Enter coordinates of C (x3 y3): ");
-    scanf("%lf %lf", &x3, &y3);
-
-    printf("Enter coordinates of point P (x y): ");
-    scanf("%lf %lf", &x, &y);
-
-    if (isInside(x1, y1, x2, y2, x3, y3, x, y))
-        printf("Point lies inside the triangle.\n");
-    else
-        printf("Point lies outside the triangle.\n");
-
-    return 0;
-}
+ int x1, x2, x3, y1, y2, y3, x, y;
+ int x1, x2, x3, y1, y2, y3, x, y;
+ float A1, A2, A3, A;
+ printf("Enter the cordinates of first point of the trianlge : ");
+ scanf("%d%d", &x1, &y1);
+ printf("\n\nEnter the cordinates of second point of the trianlge : ");
+ scanf("%d%d", &x2, &y2);
+ printf("\n\nEnter the cordinates of third point of the trianlge : ");
+ scanf("%d%d", &x3, &y3);
+ printf("\n\nEnter the points to check it's position : ");
+ scanf("%d%d", &x, &y);
+ A1 = ar(x1, y1, x2, y2, x, y);
+ printf("A1 : %f\n", A1);
+ A2 = ar(x1, y1, x3, y3, x, y);
+ printf("A2 : %f\n", A2);
+ A3 = ar(x, y, x2, y2, x3, y3);
+ printf("A3 : %f\n", A3);
+ A = ar(x1, y1, x2, y2, x3, y3);
+ printf("A : %f\n", A);
+ if (!(A1 + A2 + A3>A || A1 + A2 + A3<A))
+ printf("\n\nThe point (%d,%d) lies inside of triangle.", x, y);
+ else
+ printf("\n\nThe point (%d,%d) lies outside of triangle.", x, y);
+ getch();
+ return 0;
+ }
+ float dis(int x1, int y1, int x2, int y2)
+ {
+ float distance;
+ distance = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
+ return (distance);
+ }
+ float ar(int x1, int y1, int x2, int y2, int x3, int y3)
+ {
+ float a, b, c, area, S;
+ a = dis(x1, y1, x2, y2);
+ b = dis(x1, y1, x3, y3);
+ c = dis(x2, y2, x3, y3);
+ S = (a + b + c) / 2;
+ area = sqrt(S*(S - a)*(S - b)*(S - c));
+ return (area);
+ }
