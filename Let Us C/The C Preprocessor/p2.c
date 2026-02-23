ea2a2c57b9a75256576ff83d83f0160da198d8f4
smitrabari <smitrabari18@gmail.com>
2026-02-23 20:41:26 +0530

    Trails 4

--- Let Us C/The C Preprocessor/p2.c
+++ Let Us C/The C Preprocessor/p2.c
@@ -1,6 +1,6 @@
 // calculator for finding perimeter and area of circle, square and triangle using function like macros  
 #include<stdio.h>
-#define PI 3.14 
+#define PI 3.14 // function like macros
 #define pericle(r) (2*PI*r)
 #define areacircle(r) (PI*r*r)
 #define perisquare(a) (4*a)
