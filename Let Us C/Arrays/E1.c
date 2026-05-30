f592ecc47ac10ffbe3c39659a5f666aef3ab7e5f
smitrabari <smitrabari18@gmail.com>
2026-05-30 13:11:40 +0530

    Trails 00

--- Let Us C/Arrays/E1.c
+++ Let Us C/Arrays/E1.c
@@ -1,14 +1,15 @@
+// taking 25 numbers from user and counting total positive, negative, even and odd numbers
 #include <stdio.h>
 int main() 
 {
-    int num[25];
+    int num[25]; // array to store 25 numbers
     int pos=0,neg=0,even=0,odd=0;
-    for(int i = 0;i<=24;i++)
+    for(int i = 0;i<=24;i++) // loop to take input from user
     {
         printf("Enter %d number - ",i+1);
         scanf("%d",&num[i]);
     }
-    for(int i = 0;i<=24;i++)
+    for(int i = 0;i<=24;i++) // loop to count positive and negative numbers
     {
         if(num[i]<0)
         {
@@ -19,7 +20,7 @@ int main()
             pos++;
         }
     }
-    for(int i = 0;i<=24;i++)
+    for(int i = 0;i<=24;i++) // loop to count even and odd numbers
     {
         if(num[i]%2==0)
         {
